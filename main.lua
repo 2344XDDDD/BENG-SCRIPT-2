2537ce85be9836c9e3517213a3ee8f12f1049346
2344XDDDD <2176671594@qq.com>
2026-01-26 00:10:47 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,9 +4,7 @@
     | |/ |/ / / _ \/ _  / /_/ // /  
     |__/|__/_/_//_/\_,_/\____/___/
     
-    v1.6.69  |  2026-01-25  |  Roblox UI Library for scripts
-    
-    To view the source code, see the `src/` folder on the official GitHub repository.
+    v1.6.7  |  2026-01-26  |  Roblox UI Library for scripts
     
     Author: Footagesus (Footages, .ftgs, oftgs)
     Github: https://github.com/Footagesus/WindUI
@@ -11541,6 +11539,8 @@ CornerRadius=UDim.new(0,au.UICorner)
 }),
 au.UIElements.SideBarContainer,
 au.UIElements.MainBar,
+
+am("TextLabel",{Name="Footer",Text="WindUI Forked by orialdev | 1.0.0",TextSize=13,TextTransparency=0.7,TextXAlignment="Right",BackgroundTransparency=1,Position=UDim2.new(1,-14,1,-14),AnchorPoint=Vector2.new(1,1),Size=UDim2.new(0,0,0,0),AutomaticSize="XY",ThemeTag={TextColor3="Text",},FontFace=Font.new(al.Font,Enum.FontWeight.Medium)}),
 
 aB,
 
